ccbc8a51d687fa7bf0f10eec29bee6a3d0f6e9b3
Sebastian Sanabria <ssebastiang97@gmail.com>
2024-03-09 20:32:31 -0500

    añadir i18n

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import App from './App.tsx';
+import '@fontsource/lexend-zetta/300.css';
+import './styles/styles.ts'
+import './libs/i18n/i18n.ts'
+import { Suspense } from "react";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Suspense fallback="Cargando traducciones...">
+        <App />
+      </Suspense>
     </BrowserRouter>
   </React.StrictMode>,
 )
