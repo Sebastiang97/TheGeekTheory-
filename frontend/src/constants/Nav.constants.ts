5f0f6f045f246d27dad92bbfeb46c20524000348
Sebastian Sanabria <ssebastiang97@gmail.com>
2024-05-26 19:15:37 -0500

    Add Nav List Constants

---
+++ frontend/src/constants/Nav.constants.ts
@@ -0,0 +1,18 @@
+export const NAV_LIST = Object.freeze([
+    {
+        text: "Home",
+        to: ""
+    },
+    {
+        text: "¿Quienes somos?",
+        to: ""
+    },
+    {
+        text: "Diseños",
+        to: ""
+    },
+    {
+        text: "Prendas",
+        to: ""
+    }
+])
