dd8516d62635e37dc3ccf44950dce9cf779dafd8
Sebastian Sanabria <ssebastiang97@gmail.com>
2024-06-22 20:34:43 -0500

    Add Sidebar Component

--- frontend/src/components/SideBar/SideBar.tsx
+++ frontend/src/components/SideBar/SideBar.tsx
@@ -1,43 +1,51 @@
-import {
-    Sheet,
-    SheetContent,
-    SheetHeader,
-    SheetTitle,
-  } from "@/components/ui/sheet"
-import { ADMIN_MENU } from "./sideBar.constants"
-import { Link } from "react-router-dom"
-import { useSideBarStore } from "@/libs/store/zustand/useSideBar";
+import { NavLink } from "react-router-dom"
+import { toast } from "sonner"
+import { SheetComponent } from "@@/SheetComponent/SheetComponent"
+import { useSideBarStore } from "@/libs/store/zustand/useSideBar"
+import { NAVBAR_LIST } from "@/constants/Nav.constants"
+
+import "./sidebar.css"
 
 export const SideBar = () => {
-  const isOpen = useSideBarStore(state => state.isOpen)
   const toggle = useSideBarStore(state => state.toggle)
 
   return (
     <div className="container">
-      <div className="adminTitle">
-        <Sheet open={isOpen} onOpenChange={toggle}>
-          <SheetContent className="bg-black" side="left">
-            <SheetHeader >
-              <SheetTitle className="text-white">Admin Menu</SheetTitle>
-            </SheetHeader>
-            <div className="text-white">
-              <ul>
-                  {
-                      ADMIN_MENU.map((el, i)=>(
-                          <li key={i}>
-                              <Link 
-                                onClick={toggle}
-                                to={el.redirect}>
-                                {el.label}
-                              </Link>
-                          </li>
-                      ))
-                  }
-              </ul>
-            </div>
-          </SheetContent>
-        </Sheet>
-      </div>
+      <SheetComponent
+        classNameContent="bg-black"
+        content={
+          <div className="sidebar">
+            <ul>
+              {
+                NAVBAR_LIST.map((el, i) => (
+                  <li className="menuItem" key={i}>
+                    <NavLink
+                      onClick={toggle}
+                      to={el.to}
+                    >
+                      {el.text}
+                    </NavLink>
+                  </li>
+                ))
+              }
+              <button
+                className="mt-6"
+                onClick={() =>
+                  toast("Event has been created", {
+                    description: "Sunday, December 03, 2023 at 9:00 AM",
+                    action: {
+                      label: "Undo",
+                      onClick: () => console.log("Undo"),
+                    },
+                  })
+                }
+              >
+                Show Toast
+              </button>
+            </ul>
+          </div>
+        }
+      />
     </div>
   )
 }
