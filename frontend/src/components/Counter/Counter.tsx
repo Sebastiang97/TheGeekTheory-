b49d189a476394dc3a9f717d9d597cba42a8b744
Sebastian Sanabria <ssebastiang97@gmail.com>
2024-09-03 12:38:45 -0500

    Add counter

--- frontend/src/components/Counter/Counter.tsx
+++ frontend/src/components/Counter/Counter.tsx
@@ -1,12 +1,33 @@
 import { useState } from "react"
 import "./counter.css"
-export const Counter = ({initialState}: {initialState:number} ) => {
-    const [count, setCount] = useState(initialState || 0)
+import { useCartStore } from "@/libs/store/zustand/useCartStore"
+import { Product } from "@/Models/Product"
+
+interface Props{ 
+  initialState: number 
+  product: Product
+}
+export const Counter = ({ initialState, product }: Props) => {
+  const [count, setCount] = useState(initialState || 0)
+  const addProduct  = useCartStore(state => state.addProduct)
+
+  const addCart =()=>{
+    addProduct(product, count)
+    setCount(1) 
+  }
   return (
-    <div className="counter">
-        <button onClick={()=>{setCount((current:number)=> current - 1)}}>-</button>
+    <>
+      <div className="add">
+        <button
+          onClick={() => addCart()}>
+          Agregar a tu carrito
+        </button>
+      </div>
+      <div className="counter">
+        <button onClick={() => { setCount((current: number) => current - 1) }}>-</button>
         <div className="display">{count}</div>
-        <button onClick={()=>{setCount((current:number)=> current + 1)}}>+</button>
-    </div>
+        <button onClick={() => { setCount((current: number) => current + 1) }}>+</button>
+      </div>
+    </>
   )
 }
